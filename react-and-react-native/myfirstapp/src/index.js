6c8122abac3fcbc0c1deb43593958aeae60612fd
AffirmedVisionary <iam@charlenegrant.co.uk>
2020-10-09 12:59:20 +0100

    learning forms in react s6

--- react-and-react-native/myfirstapp/src/index.js
+++ react-and-react-native/myfirstapp/src/index.js
@@ -1,17 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const LogForm = () => {
+  const [name,setName] = useState('');
+  const [age,setAge] = useState("");
+  const [location,setLocation] = useState("");
+  const [hasSubmitted, setSubmitted] = useState(false);
+ 
+  function handleSubmit(event) {
+    event.preventDefault();
+    setSubmitted(true);
+    // alert('A name was submitted: ' + name);
+  }
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    return (
+      <React.Fragment>
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <label>
+          Name:
+          <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+        </label>
+
+        <label>
+          Age:
+          <input type="number" value={age} onChange={(e) => setAge(e.target.value)} />
+        </label>
+
+        <label>
+          Location:
+          <input type="text" value={location} onChange={(e) => setLocation(e.target.value)} />
+        </label>
+
+
+        <input type="submit" value="Submit" />
+      </form>
+     {hasSubmitted && (<div>
+        {name}
+        <br></br>
+        {age}
+        <br></br>
+        {location}
+        <br></br>
+        <p>Thank you for submitting your form</p>
+      </div>
+      )}
+      </React.Fragment>
+    );
+}
+
+ReactDOM.render(<LogForm />,document.getElementById('root'));
