b579b6040f7b6527d0397b9e40b0ec28a2883d03
AffirmedVisionary <iam@charlenegrant.co.uk>
2020-10-13 13:17:52 +0100

    Updates

---
+++ precache-manifest.ab6f1f665d295e7c7da1406e9afc7f95.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be6be9cb045ec6b14114a09cb2838d9a",
+    "url": "/blackcodher-bootcamp/react-and-react-native/mybookcase2/index.html"
+  },
+  {
+    "revision": "fd1c1733ae08e46ff732",
+    "url": "/blackcodher-bootcamp/react-and-react-native/mybookcase2/static/css/main.1c91e437.chunk.css"
+  },
+  {
+    "revision": "e33b0be681b99525a3b4",
+    "url": "/blackcodher-bootcamp/react-and-react-native/mybookcase2/static/js/2.8b982c36.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/blackcodher-bootcamp/react-and-react-native/mybookcase2/static/js/2.8b982c36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd1c1733ae08e46ff732",
+    "url": "/blackcodher-bootcamp/react-and-react-native/mybookcase2/static/js/main.c362fb69.chunk.js"
+  },
+  {
+    "revision": "f7baeedf8d191d85a227",
+    "url": "/blackcodher-bootcamp/react-and-react-native/mybookcase2/static/js/runtime-main.bd50c428.js"
+  }
+]);
